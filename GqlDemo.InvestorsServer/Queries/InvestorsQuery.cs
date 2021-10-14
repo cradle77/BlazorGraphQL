907cdecd275f6bf140685a49be8108ab316427b2
MarcoDes <marcodesanctis2@gmail.com>
2021-10-14 16:15:33 +0100

    port

--- GqlDemo.InvestorsServer/Queries/InvestorsQuery.cs
+++ GqlDemo.InvestorsServer/Queries/InvestorsQuery.cs
@@ -11,9 +11,9 @@ namespace GqlDemo.InvestorsServer.Queries
         {
             return new[] 
             {
-                new Investor() { Id = 1, Name = "Marco", NetWorth = 1000000, IndustryId = 1 },
-                new Investor() { Id = 1, Name = "Daniele", NetWorth = 5000000, IndustryId = 2 },
-                new Investor() { Id = 1, Name = "Andrea", NetWorth = 3000000, IndustryId = 2 }
+                new Investor() { Id = 1, Name = "John", NetWorth = 1000000, IndustryId = 1 },
+                new Investor() { Id = 1, Name = "Daniel", NetWorth = 5000000, IndustryId = 2 },
+                new Investor() { Id = 1, Name = "Andrew", NetWorth = 3000000, IndustryId = 2 }
             }.AsQueryable();
         }
     }
